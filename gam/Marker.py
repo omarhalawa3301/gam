beca9d13fde1c9b4c1859054abc6d2ac4c519903
Omar Halawa <113780419+omarhalawa3301@users.noreply.github.com>
2023-05-26 11:47:58 -0700

    TODO, review purpose of Marker.py

--- gam/Marker.py
+++ gam/Marker.py
@@ -6,7 +6,8 @@
     File name:     Marker.py
     Project:       GAM (GWAS with Advanced Machine-learning) - CSE 185 Project
     Repository:    https://github.com/omarhalawa3301/gam
-    Description:   GAM marker string python-containing script                   
+    Description:   GAM marker string python-containing script.
+                   # TODO: Review purpose of this file?
     References:    https://www.cog-genomics.org/plink/1.9/assoc#linear
 """
 
